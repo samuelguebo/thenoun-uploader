4f2f30bb74bc190395433e68c218541623deecc1
Samuel Guebo <samuelguebo@gmail.com>
2019-08-18 19:33:26 +0000

    [IMPR] Decouple code and connect with API

--- js/script.js
+++ js/script.js
@@ -10,32 +10,57 @@
 'use strict'
 
 // variables
-var btnSearch = document.getElementById( "btn-search" );
-var btnSearch = document.getElementById( "btn-search" );
+var apiUrl = "http://localhost/php-restapi-vanilla"
 var form = document.getElementById( "results-form" )
 var table = document.getElementById( "results-table" ).querySelector( "tbody" )
-var deletions = document.getElementsByClassName( "action-delete" )
 
-// Listen to "Create new item" click
-btnSearch.addEventListener( 'click',
-function( e ){
-  var username = form.querySelector( "input[name=username]" ).value;
-  if( username !== "" ){
-    form.querySelector( "input[name=username]" ).value = ""
+handleCreation();
 
-    // build HTML rows
-    var count = table.childNodes.length
-    var row = document.createElement( "tr" )
-    var today = new Date()
-    var rowHTML = "<td>" + ( count++ ) + "</td>"
-        rowHTML += "<td>" + username + "</td>"
-        rowHTML += "<td>" + today.getDate() + "-" + ( today.getMonth() + 1 )
-        rowHTML += "-" + today.getFullYear() + "</td>"
-        rowHTML += "<td><i class='btn btn-small btn-primary action-delete'>"
-        rowHTML += "x</i></td>"
-    row.innerHTML = rowHTML
-    table.append( row )
+/**
+ * Wrapper function for handling
+ * the insertion of new records
+ */
+function handleCreation() {
+  
+  // Listen to "Create new item" click
+  var btnSearch = document.getElementById( "btn-search" );
+  btnSearch.addEventListener( 'click',
+  
+  function( e ){
+    var title = form.querySelector( "input[name=title]" ).value;
+    if( title !== "" ){
 
+        /* Make call to API and change table
+          if the request is successful */
+          fetch(apiUrl + "/product", {
+            method: "POST",
+            body: { title: username, status: "PENDING" }
+          }).then( response => response.json())
+          .then((product) => {
+
+            // append product to table
+            addRowToTable(product)
+            
+            // clear input content
+            form.querySelector( "input[name=title]" ).value = ""
+
+            // re-attach deletion listener
+            handleDeletion();
+
+
+          }) 
+         
+    }
+    e.preventDefault();
+  })
+
+  /**
+   * Wrapper function for handling
+   * deletions of item list from 
+   * the UI
+   */
+  function handleDeletion() {
+    var deletions = document.getElementsByClassName( "action-delete" )
     // Listen to deletion buttion clicks
     for ( var i = 0; i < deletions.length; i++ ) {
       var tableRow = deletions[i];
@@ -45,6 +70,22 @@ function( e ){
         this.closest( 'tr' ).remove()
       })
     }
+
   }
-  e.preventDefault();
-})
+
+  /**
+   * Append row to the table
+   */
+  function addRowToTable(product) {
+    // build HTML rows
+    var count = table.childNodes.length
+    var row = document.createElement( "tr" )
+    var rowHTML = "<td>" + ( count++ ) + "</td>"
+        rowHTML += "<td>" + product.title + "</td>"
+        rowHTML += "<td>" + product.pubDate + "</td>"
+        rowHTML += "<td><i class='btn btn-small btn-primary action-delete'>"
+        rowHTML += "x</i></td>"
+    row.innerHTML = rowHTML
+    table.append( row )
+  }
+}
