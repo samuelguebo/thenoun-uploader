1949b0cd323f7fc003e6f47708a16952ddb91bf3
Samuel Guebo <samuelguebo@gmail.com>
2020-05-10 05:17:46 +0000

    [IMPR] Authentication middleware

---
+++ app/controllers/auth-middleware.js
@@ -0,0 +1,23 @@
+// Applying authentication to specific endpoints
+var express = require('express');
+var router = express.Router();
+const path = require('path')
+
+// Protecting /users path
+router.use('/', function (req, res, next) {
+    let isLoggedIn = req.session.is_loggedin
+    let allowedRoutes = ['/login', '/oauth-callback', '/logout']
+    // decode token
+    console.log(`isLoggedIn: ${isLoggedIn}`)
+    if (isLoggedIn) {
+        next();
+    } else {
+        if (allowedRoutes.indexOf(req.path) > -1) {
+            next();
+        } else {
+            res.sendFile(path.join(__dirname + '/../../logged-out.html'));
+        }
+    }
+});
+
+module.exports = router;
