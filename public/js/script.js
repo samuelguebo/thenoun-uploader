e2f05d463f98be50dd3082656a1a418158234c44
Samuel Guebo <samuelguebo@gmail.com>
2020-07-12 23:52:26 +0000

    [IMPR] Override Pond upload with a custom the logic

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,79 @@
 /**
  * Create a file uploader
+ * @link https://pqina.nl/filepond/docs/patterns/api/server/
  */
-
+const uploadURI = './upload' // backend endpoint
 const pond = FilePond.create(
-    document.querySelector('input.filepond')
+    document.querySelector('input.filepond'), {
+        acceptedFileTypes: ['image/svg'],
+        maxFileSize: '500KB',
+
+    }
 );
 
 /**
- * Hide and show upload button
+ * Improve UX by hiding or dislaying
+ * the upload button
  */
 const displayUploadButton = () => {
     let uploadButton = document.getElementById("upload-button")
     // deal with each file
     pond.on('addfile', (error, file) => {
-        if (error) {
-            console.log('Oh no');
-            return;
-        }
-        //console.log('File added', file);
         uploadButton.style.display = 'block';
     });
 
     pond.on('removefile', (error, file) => {
         let files = pond.getFiles()
-
         // Hide upload button
-        if (files.length < 1) {
+        if (files.length < 1)
             uploadButton.style.display = 'none';
-        }
-        //console.log('File added', file);
+    });
 
+    uploadButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        uploadToServer(); // push to backend
+    })
+}
+
+/**
+ * Override Pond upload with a custom the logic
+ * for sending icons to server for further 
+ * processing (i.e: send to wiki, etc.)
+ */
+const uploadToServer = () => {
+    // override Pond upload
+    pond.setOptions({
+        server: {
+            url: uploadURI,
+            // disable asynchronous upload, just return the file
+            process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+
+                // done processing files
+                let formData = new FormData();
+                formData.append(fieldName, file, file.name)
+
+                // post data
+                fetch(uploadURI, {
+                        body: formData,
+                        method: 'POST'
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log("upload response", data)
+                        load(200);
+                    }).catch(error => load(300))
+
+            }
+        }
     });
 }
 
-displayUploadButton()
+/**
+ * Centralize event listeners for code readability.
+ * Consider a better approach to code organization
+ */
+const initListeners = () => {
+    displayUploadButton()
+}
+
+initListeners() // trigger event listeners
