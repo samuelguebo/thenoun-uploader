3e8ecf58bc5c759651c67cb7a1af0a78fd69ca99
Samuel Guebo <samuelguebo@gmail.com>
2020-07-12 00:56:29 +0000

    [IMPR] Show and hide upload button conditionally

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,36 @@
-import {
-    uploadArea,
-    uploadInput,
-    preventDropConflict
-} from './fields.js'
+/**
+ * Create a file uploader
+ */
 
-uploadArea()
-uploadInput()
-preventDropConflict()
+const pond = FilePond.create(
+    document.querySelector('input.filepond')
+);
+
+/**
+ * Hide and show upload button
+ */
+const displayUploadButton = () => {
+    let uploadButton = document.getElementById("upload-button")
+    // deal with each file
+    pond.on('addfile', (error, file) => {
+        if (error) {
+            console.log('Oh no');
+            return;
+        }
+        //console.log('File added', file);
+        uploadButton.style.display = 'block';
+    });
+
+    pond.on('removefile', (error, file) => {
+        let files = pond.getFiles()
+
+        // Hide upload button
+        if (files.length < 1) {
+            uploadButton.style.display = 'none';
+        }
+        //console.log('File added', file);
+
+    });
+}
+
+displayUploadButton()
