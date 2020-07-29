44c9f2f7a7adff5a0be99bba82e4e428e16e92c7
samuelguebo <samuelguebo@gmail.com>
2020-07-29 22:47:51 +0000

    [STYLE] Use CDN files from ToolForge server

--- src/Views/index.php
+++ src/Views/index.php
@@ -14,10 +14,10 @@
   <title> <?php echo APP_NAME; ?> | <?php echo APP_DESCRIPTION; ?></title>
 
   <!-- Bootstrap -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="//tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//tools-static.wmflabs.org/cdnjs/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Filepond stylesheet -->
-  <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
+  <link href="//tools-static.wmflabs.org/cdnjs/ajax/libs/filepond/4.18.0/filepond.min.css" rel="stylesheet">
   <!-- Default stylesheet -->
   <link href="public/css/style.css" rel="stylesheet">
 </head>
@@ -78,11 +78,11 @@
 	  </section>
 
 	</section>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/3.2.1/jquery.slim.min.js"></script>
+	<script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	<!-- Load FilePond library -->
-	<script src="https://unpkg.com/filepond/dist/filepond.js"></script>
+	<script src="//tools-static.wmflabs.org/cdnjs/ajax/libs/filepond/4.18.0/filepond.min.js"></script>
 	<!-- Turn all file input elements into ponds -->
 	<script src="public/js/icon.js"></script>
 	<script src="public/js/script.js"></script>
