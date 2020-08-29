c33497285d9647e0935168828109cda6438c4115
samuelguebo <samuelguebo@gmail.com>
2020-08-29 23:50:22 +0000

    [TEST] Unit test for MediaWiki helper

---
+++ tests/MediaWikiTest.php
@@ -0,0 +1,27 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Thenoun\Models\Icon;
+use Thenoun\Utils\MediaWiki;
+
+class MediaWikiTest extends TestCase {
+	/**
+	 * Settings shared across all test methods
+	 *
+	 * @return void
+	 */
+	protected function setUp() : void {
+		define( 'ROOT', dirname( __DIR__ ) );
+		require_once ROOT . '/src/Config/settings.php';
+	}
+
+	/**
+	 * testIsFileExistent
+	 *
+	 * @covers \MediaWiki\
+	 * @return void
+	 */
+	public function testIsFileExistent() {
+		$file = new Icon( 'File:2010-10-10-oderpruch-pl-by-RalfR-23.jpg', null, null, null );
+		$this->assertEquals( ( new MediaWiki() )->isFileExistent( $file ), true );
+	}
+}
